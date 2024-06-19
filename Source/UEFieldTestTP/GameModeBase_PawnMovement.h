b4568f039ac42c116648988b14b279be8f81c27c
Zihan Yang <zihan_yang_1996@outlook.com>
2024-06-19 17:01:40 -0400

    Add new game mode

---
+++ Source/UEFieldTestTP/GameModeBase_PawnMovement.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameModeBase_PawnMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEFIELDTESTTP_API AGameModeBase_PawnMovement : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
